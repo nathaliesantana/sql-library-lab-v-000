cd7b778e0decf6b3e1de1478c11911643e1d2697
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-17 02:17:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,8 +20,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors 
-  WHERE author.id = subgenres.id
+  "SELECT authors.name, subgenres.name FROM authors
+  WHERE authors.id = subgenres.id
   ;"
 end
 
