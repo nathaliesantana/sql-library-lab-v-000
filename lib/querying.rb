121cbf63951b16228c69b77d9920451e7863d86b
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-17 02:02:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-  WHERE MAX(characters.motto)
+  ORDER BY LENGTH(motto) DESC LIMIT 1
   ;"
 end
 
